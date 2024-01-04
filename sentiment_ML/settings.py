147322b43bcaebec3df05c979dc4d04eab9c593d
Arena Web <monsieur.sarwar@gmail.com>
2024-01-04 14:31:07 +0600

    api issue solved

--- sentiment_ML/settings.py
+++ sentiment_ML/settings.py
@@ -38,13 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -125,3 +127,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True
--- sentiment_ML/urls.py
+++ sentiment_ML/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.predict_data, name="Predict_Data")
+    path('', views.predict_data, name="Predict_Data"),
+    path('sentml/', views.PredictData.as_view(), name="Predict_Data_api"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
